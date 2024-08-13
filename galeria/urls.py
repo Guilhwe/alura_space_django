a4f7603e2807a9807507b538cb1e07c8f4252a13
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-13 18:14:29 +0200

    correcion de los styles y assets de imagen.html y nueva ruta al clickear en una imagen de index.html

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from galeria.views import index, imagen
 
 urlpatterns = [
-    path('', index)
-    path('imagen/', imagen)
+    path('', index),
+    path('imagen/', imagen, name='imagen')
 ]
