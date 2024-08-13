58dee2da8d8d9af11acb2ddc8dbb23a589bb847a
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-13 17:48:01 +0200

    ruta url para imagen

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagen
 
 urlpatterns = [
     path('', index)
+    path('imagen/', imagen)
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 
 def index(request):
     return render (request,'galeria/index.HTML')
+def imagen(request):
+    return render (request, 'galeria/imagen.HTML')
