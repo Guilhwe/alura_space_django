3c8934d50d8bea71aef2185ecd86d9fb9d941683
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-14 17:19:31 +0200

    borrado de imaghenes en index para hacer mas manejable el codigo(luego se volveran a añadir) agregado en views el diccionario de datos con dos items y sus respesctivos dccionarios correspondientes a las 2 imagenes que se han dejado

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+datos = {
+    1:{"nombre": "Nebulosa de Carina",
+       "Leyenda": "webbtelescope.org / NASA / James Webb"},
+    2:{"nombre": "Galaxia NGC 1079",
+       "leyenda": "nasa.org / NASA / Hubble"}
+}
 
 def index(request):
     return render (request,'galeria/index.HTML')
+
 def imagen(request):
     return render (request, 'galeria/imagen.HTML')
