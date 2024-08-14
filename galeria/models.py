fde0bbfb2bbbddf00b43c617beeefc3075ab8428
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-14 18:19:51 +0200

    creacion de la clase fotografia en models. creacion y migracios de los datos de models a migrations 0001 usando el banco de datos de django. todo esto es para el uso de un banco de datos en sql a traves de django

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+    nombre = models.CharField(max_length=100, null= False, blank=False)
+    #charfield para que el campo sea una string con el maximo de caracteres que quieres como nombre NULL false y blankfalse para aseguirar que haya algo escrito
+    leyenda = models.CharField(max_length=150, null= False, blank=False)
+    descripcion = models.TextField(null= False, blank=False)
+    #Textfield porque es unn texto mas largo y elaborado, sin limite de caracteres pero asegurando cubrir el null y blank
+    foto= models.CharField(max_length=100, null= False, blank=False)
+
+    def __str__(self):
+        return f"Fotografia [nombre={self.nombre}]"
