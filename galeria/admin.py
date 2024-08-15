c7fcb3714a767c7b69f73f93c4055c05a57d9094
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-15 17:30:35 +0200

    implementacionde un parametro de busqueda por nombre  en django  admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -4,6 +4,7 @@ from galeria.models import Fotografia
 class ListandoFotografias(admin.ModelAdmin):
     list_display= ("id", "nombre", "leyenda")
     list_display_links = ("id", "nombre")
+    search_fields = ("nombre",)
 
 admin.site.register(Fotografia, ListandoFotografias)
 
