2f657ebfe2a4cd1afec641da6f1ab70cc1bfdad8
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-15 17:37:16 +0200

    ampliacion de models para añadir a la base de datos el campo de tags(categorias). se igualan las opciones a una lista cerrada que solo el admin puede manipular

--- galeria/models.py
+++ galeria/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 class Fotografia(models.Model):
+
+    opciones_de_categoria = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELLA", "Estrella"),
+        ("GALAXIA", "Galaxia"),
+        ("PLANETA", "Planeta")
+
+    ]
+
     nombre = models.CharField(max_length=100, null= False, blank=False)
     #charfield para que el campo sea una string con el maximo de caracteres que quieres como nombre NULL false y blankfalse para aseguirar que haya algo escrito
     leyenda = models.CharField(max_length=150, null= False, blank=False)
+    categoria = models.CharField(max_length =100, choices=opciones_de_categoria, default='')
     descripcion = models.TextField(null= False, blank=False)
     #Textfield porque es unn texto mas largo y elaborado, sin limite de caracteres pero asegurando cubrir el null y blank
     foto= models.CharField(max_length=100, null= False, blank=False)
