7299427e2c3215a0b50f03cc56a463436f3702cd
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-15 17:51:55 +0200

    añadido de la paginacion para ver x items por pagina en el admin para facilitar la navegacion en el banco de datos. creacion de un nuevo campo en models para evitar que fotos sin toda la informacion sean publicadas.

--- galeria/admin.py
+++ galeria/admin.py
@@ -6,6 +6,7 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display_links = ("id", "nombre")
     search_fields = ("nombre",)
     list_filter = ("categoria",)
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
 
