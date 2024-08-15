bdac6b2976b16f40a7e110d505435bd7d6853bf0
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-15 17:43:02 +0200

    añadido del buscador por categoria mediante el list_filter en el admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -5,6 +5,7 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display= ("id", "nombre", "leyenda")
     list_display_links = ("id", "nombre")
     search_fields = ("nombre",)
+    list_filter = ("categoria",)
 
 admin.site.register(Fotografia, ListandoFotografias)
 
