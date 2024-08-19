32e3ca80a11b0cea63d3ebc8219a52278b8d343a
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-19 17:37:05 +0200

    creacion de los html para login y registro. creacion de los metodfos en views de login y registro. añadido de los paths en urls.py de usuarios para los metodos de login y registro

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
+from usuarios.views import login, registro
 
 urlpatterns =[
+    path('login', login, name='login'),
+    path('registro', registro, name='registro'),
     
+
 ] 
    
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    return render (request, 'usuarios/login.html')
+
+def registro(request):
+   return render (request, 'usuarios/registro.html')
