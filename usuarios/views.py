8e05c3c2b50d15d70773a1b07ca7050a96a2e4b3
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-19 19:11:32 +0200

    creacion de forms.py y modificaciones menores en textos

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,4 +4,4 @@ def login(request):
     return render (request, 'usuarios/login.html')
 
 def registro(request):
-   return render (request, 'usuarios/registro.html')
+    return render (request, 'usuarios/registro.html')
