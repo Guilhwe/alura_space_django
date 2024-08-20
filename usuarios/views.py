615254e77873c45b42510042be50b50f576d9760
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-20 20:41:28 +0200

    cambio en la logica del codigo para que el usuario credo no sea igual a otro que ya existe y que se guarde en la base de datos que nos da django

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, RegistroForms
+from django.contrib.auth.models import User
 
 def login(request):
     form=LoginForms()
@@ -7,4 +8,26 @@ def login(request):
 
 def registro(request):
     form = RegistroForms()
+
+    if request.method == 'POST':
+        form =RegistroForms(request.POST)
+
+        if form.is_valid():
+             if form['contraseña1'].value () != form['contraseña2'].value():
+                return redirect('registro')
+             
+        nombre = form['nombre_registro'].value()
+        email = form['email'].value()
+        contraseña = form['contraseña1'].value()
+        
+        if User.objects.filter(username=nombre).exists():
+             return redirect('registro')
+        usuario = User.objects.create_user(
+                username = nombre,
+                email=email,
+                password=contraseña,
+                )
+        usuario.save()
+        return redirect('login')
+
     return render (request, 'usuarios/registro.html', {"form" : form})
