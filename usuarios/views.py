7c16647e51144621d93a459b63462855e9a8fa70
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-20 21:23:07 +0200

    alertas de exito y error con su respectiva estilizacion mediante bootstrap

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, RegistroForms
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib import messages
 
 def login(request):
     form=LoginForms()
@@ -10,16 +11,20 @@ def login(request):
         form = LoginForms(request.POST)
         if form.is_valid():
             nombre=form['nombre_login'].value()
+            
             contraseña= form['contraseña'].value()
         usuario= auth.authenticate(
             request,
             username=nombre,
             password= contraseña
+            
             )
         if usuario is not None:
             auth.login(request,usuario)
+            messages.success(request,'f{nombre} logeado con succeso')
             return redirect('index')
         else:
+            messages.error(request,'ERROR de login')
             return redirect('login')
 
 
@@ -37,6 +42,7 @@ def registro(request):
         if form.is_valid():
              #para verificar si el formulario es valido
              if form['contraseña1'].value () != form['contraseña2'].value():
+                messages.error(request,'Las contraseñas no coinciden')
                 return redirect('registro')
              #verficar si las contraseñas son iguales
              
@@ -46,6 +52,7 @@ def registro(request):
         #guardar las informaciones del formularia en otras variables para hacerlo  mas manejable
         
         if User.objects.filter(username=nombre).exists():
+             messages.error(request, 'El usuario ya está registrado')
              return redirect('registro')
         #verifica si el usuario ya exite
         usuario = User.objects.create_user(
@@ -54,6 +61,7 @@ def registro(request):
                 password=contraseña,
                 )
         usuario.save()
+        messages.success(request, 'Registrado con éxito')
         #crea nuevo usuario
         return redirect('login')
     #redirecciona a login
