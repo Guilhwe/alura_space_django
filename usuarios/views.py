0b1e93d0ffd575b2e644385d52d633e260d5a535
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-20 20:54:05 +0200

    coments de aclaracion para revision

--- usuarios/views.py
+++ usuarios/views.py
@@ -12,22 +12,30 @@ def registro(request):
     if request.method == 'POST':
         form =RegistroForms(request.POST)
 
+        
         if form.is_valid():
+             #para verificar si el formulario es valido
              if form['contraseña1'].value () != form['contraseña2'].value():
                 return redirect('registro')
+             #verficar si las contraseñas son iguales
              
         nombre = form['nombre_registro'].value()
         email = form['email'].value()
         contraseña = form['contraseña1'].value()
+        #guardar las informaciones del formularia en otras variables para hacerlo  mas manejable
         
         if User.objects.filter(username=nombre).exists():
              return redirect('registro')
+        #verifica si el usuario ya exite
         usuario = User.objects.create_user(
                 username = nombre,
                 email=email,
                 password=contraseña,
                 )
         usuario.save()
+        #crea nuevo usuario
         return redirect('login')
+    #redirecciona a login
+    
 
     return render (request, 'usuarios/registro.html', {"form" : form})
